319991d87b1d18856dd7ae39276912c03e6af04d
Evah0 <evajosam464@gmail.com>
2024-12-09 01:35:20 +0300

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&vZE1QL7K#dAl-i220BihDAtq03qw1MEjlYIvYAi2eX3m5PrgYPzQ",
 };
